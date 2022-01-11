6f6eb5ac47d4a74c6b093e1c769a58339612579e
Gompei <shoueigonpei6028@gmail.com>
2022-01-11 17:19:00 +0900

    リファクタリング

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# [line-coding-test](https://github.com/line/recruiting-resources-jp/tree/main/techtrain/1st-mission)
+# line-coding-test
 
 [![ci](https://github.com/Gompei/line-coding-test/actions/workflows/ci.yml/badge.svg)](https://github.com/Gompei/line-coding-test/actions/workflows/ci.yml)
+
+## 概要
+
+LINEコーディングテスト体験
+
+## 参考
+
+- [ザ・ミッション: タクシーの運賃を求めろ！](https://github.com/line/recruiting-resources-jp/tree/main/techtrain/1st-mission)
--- cmd/line-coding-test/main.go
+++ cmd/line-coding-test/main.go
@@ -11,29 +11,17 @@ import (
 	"time"
 )
 
-// TODO: 変数名の統一性
 const (
 	// TimeFormat 走行ログの時間フォーマット
 	TimeFormat = "2006-01-02 15:04:05.000"
-	// BaseFare 初乗り料金(単位:円)
-	BaseFare = 410
 	// AdditionalFare 追加運賃(単位:円)
 	AdditionalFare = 80
-	// FareAdditionDistance 運賃加算距離(単位:m)
-	FareAdditionDistance = 237
-	// MaxTravelLogCount タクシー最大走行ログ件数(1乗客)
-	MaxTravelLogCount = 500000
-	// FirstRideDistance 初乗り料金最大走行距離(単位:km)
-	FirstRideDistance = 1052.0
-	// LowSpeedDrivingStandard 低速走行基準(単位:10km/h)
-	LowSpeedDrivingStandard = 10.0
-	// SlowFareAdditionTime 低速運賃加算時間(単位:秒)
-	SlowFareAdditionTime = 90.0
 )
 
 func main() {
 	// 走行ログ取得(最大5万行なので、あらかじめメモリを確保しておく)
-	driveLogs := make([]string, 0, MaxTravelLogCount)
+	// Scan後に計算しても良いが、今回は別関数に切り出し
+	driveLogs := make([]string, 0, 500000)
 	reader := bufio.NewScanner(os.Stdin)
 	for reader.Scan() {
 		driveLogs = append(driveLogs, reader.Text())
@@ -43,7 +31,7 @@ func main() {
 	}
 
 	// 運賃計算
-	fare, err := calculateTaxiFare(driveLogs)
+	fare, err := calcTaxiFare(driveLogs)
 	if err != nil {
 		log.Fatalf("Failed to calculate taxi fare: %v", err)
 	}
@@ -51,92 +39,106 @@ func main() {
 	fmt.Println(fare)
 }
 
-// calculateTaxiFare タクシー走行ログを元に、乗車料金を算出する
-func calculateTaxiFare(driveLogs []string) (int, error) {
-	// 運賃
-	fare := BaseFare
-	// 走行距離, 低速走行時間
-	var mileage, lowSpeedRunningTime float64
-	// 前レコード(1レコード分)
-	var PreviousDriveLog []string
+// calcTaxiFare タクシー走行ログを元に、乗車料金を算出する
+func calcTaxiFare(driveLogs []string) (int, error) {
+	// 走行距離, 低速走行総時間
+	var totalMileage, lowSpeedDrivingTotalTime float64
+	// 前レコードの走行時刻,距離
+	var previousDriveTime time.Time
+	var previousDriveMileage float64
 
 	// 走行距離,低速走行時間算出
 	for _, v := range driveLogs {
-		travelLog := strings.Split(v, " ")
+		driveLog := strings.Split(v, " ")
 
 		// 走行時刻
-		ti, err := toTime(travelLog[0])
+		driveTime, err := toTime(driveLog[0])
 		if err != nil {
 			return 0, err
 		}
 
 		// 移動距離
-		mi, err := strconv.ParseFloat(travelLog[1], 64)
+		mileage, err := strconv.ParseFloat(driveLog[1], 64)
 		if err != nil {
 			return 0, err
 		}
 
 		// 深夜料金計算
-		// 00:00:00.000 〜 04:59:59.999 又は 22:00:00.000 〜 23:59.59.999
-		hour := ti.Hour()
-		if (5 > hour && hour >= 0) || (24 > hour && hour >= 22) {
-			mileage += mi * 1.25
+		if isMidnight(driveTime.Hour()) {
+			totalMileage += mileage * 1.25
 		} else {
-			mileage += mi
+			totalMileage += mileage
 		}
 
-		if mileage != 0.0 && len(PreviousDriveLog) > 0 {
-			f, err := strconv.ParseFloat(PreviousDriveLog[1], 64)
-			if err != nil {
-				return 0, err
-			}
-			// 移動距離(km)
-			travelDistance := (mi - f) * 0.001
-
-			t, err := toTime(PreviousDriveLog[0])
-			if err != nil {
-				return 0, err
-			}
-			duration := ti.Sub(t)
-			// 移動にかかった時間
-			travelTime := duration.Hours()
-			// 深夜料金
-			if (5 > hour && hour >= 0) || (24 > hour && hour >= 22) {
-				travelTime *= 1.25
-			}
-
-			// 平均速度
-			averageSpeed := travelDistance / travelTime
-			if averageSpeed < LowSpeedDrivingStandard {
-				lowSpeedRunningTime += duration.Seconds()
+		if mileage != 0.0 {
+			averageSpeed := calcAverageSpeed(previousDriveMileage, mileage+previousDriveMileage, previousDriveTime, driveTime)
+			// 低速走行基準: 10km/h以下
+			if averageSpeed <= 10.0 {
+				// 深夜であれば、1.25倍にする
+				s := driveTime.Sub(previousDriveTime).Seconds()
+				if isMidnight(driveTime.Hour()) {
+					s = s * 1.25
+				}
+				lowSpeedDrivingTotalTime += s
 			}
 		}
 
-		PreviousDriveLog = travelLog
+		previousDriveTime = driveTime
+		previousDriveMileage = mileage
 	}
 
 	// 距離料金計算
-	mileage -= FirstRideDistance
-	// forでは回さない
-	if mileage > 0.0 {
-		fare += AdditionalFare
-		fare += (int(mileage) / FareAdditionDistance) * AdditionalFare
-		if int(mileage)%FareAdditionDistance > 0 {
-			fare += AdditionalFare
-		}
-	}
+	fare := calcTaxiMileageFare(totalMileage)
 
 	// 低速運賃計算
-	if lowSpeedRunningTime >= SlowFareAdditionTime {
-		fare += AdditionalFare
-		lowSpeedRunningTime -= SlowFareAdditionTime
-		fare += (int(lowSpeedRunningTime) / SlowFareAdditionTime) * AdditionalFare
-		if int(lowSpeedRunningTime)%SlowFareAdditionTime > 0.0 {
-			fare += AdditionalFare
-		}
-	}
+	fare += calcTaxiLowSpeedDriveFare(lowSpeedDrivingTotalTime)
 
 	return fare, nil
+}
+
+// calcTaxiMileageFare タクシーの走行距離を元に運賃を計算する
+func calcTaxiMileageFare(mileage float64) int {
+	// 初乗り運賃
+	fare := 410
+
+	// 1052mまでは初乗り運賃で走行可能、 後237m毎に運賃加算
+	if mileage >= 1053.0 {
+		fare += (int(mileage-1053.0)/237 + 1) * AdditionalFare
+	}
+
+	return fare
+}
+
+// calcTaxiLowSpeedDriveFare タクシーの低速走行総時間を元に加算運賃を計算する
+func calcTaxiLowSpeedDriveFare(lowSpeedDrivingTotalTime float64) int {
+	// 加算運賃
+	additionalFare := 0
+
+	// 最初の89秒は無料、後90秒毎に運賃加算
+	if lowSpeedDrivingTotalTime >= 90 {
+		additionalFare += (int(lowSpeedDrivingTotalTime-90)/90 + 1) * AdditionalFare
+	}
+
+	return additionalFare
+}
+
+func calcAverageSpeed(bm, am float64, bt, at time.Time) float64 {
+	// 移動距離(km)
+	driveDistance := (am - bm) * 0.001
+
+	// 所要時間
+	time := at.Sub(bt).Hours()
+
+	return driveDistance / time
+}
+
+func isMidnight(hour int) bool {
+	// 今回の場合、24,25,26..形式での使用は想定外
+	// 0時~4時59分59秒... 又は 22時~23時59分59秒...
+	if (5 > hour && hour >= 0) || (24 > hour && hour >= 22) {
+		return true
+	}
+	return false
 }
 
 func toTime(str string) (time.Time, error) {
